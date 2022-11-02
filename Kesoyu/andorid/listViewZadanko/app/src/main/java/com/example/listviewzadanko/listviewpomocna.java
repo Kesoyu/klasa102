6b48941b5ca9e01f05062a56824a531f1802ee4b
Kesoyu <janusztm4@gmail.com>
2022-11-02 10:29:48 +0100

    googleMapsApplication

---
+++ Kesoyu/andorid/listViewZadanko/app/src/main/java/com/example/listviewzadanko/listviewpomocna.java
@@ -0,0 +1,14 @@
+package com.example.listviewzadanko;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listviewpomocna extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listviewpomocna);
+    }
+}
