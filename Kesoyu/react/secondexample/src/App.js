0f163c548d1f23f43492d55fbde0d5a01b43fdf7
Kesoyu <janusztm4@gmail.com>
2022-11-15 18:31:21 +0100

    react rooting

--- Kesoyu/react/secondexample/src/App.js
+++ Kesoyu/react/secondexample/src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import {createContext, useContext} from 'react'
+import { HashRouter, Route, Routes, Link,Outlet, Navigate, useNavigate } from 'react-router-dom';
+
+const ApplicationContext = createContext();
+
+function Example( {name} ) {
+  const appContext = useContext(ApplicationContext);
+
+  return <h1>{name}: {""+appContext}</h1>;
+}
+
+function BodyContainer(){
+  const navigate = useNavigate()
+
+  return (
+    <>
+    <ul>
+      {["strona1","strona2","strona3"].map(
+        it => <li><Link to={it}>{it}</Link></li>
+      )}
+      {["strona1","strona2","strona3"].map(
+        it => <li><button onClick={() => navigate(it)}>{it}</button></li>
+      )}
+    </ul>
+    <div style={{
+      backgroundColor: "gray"
+    }}>
+      <Outlet/>
+    </div>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Example name="poza providerem"/>
+      <ApplicationContext.Provider value={"ca"}>
+      <Example name="w providerze"/>
+
+      </ApplicationContext.Provider>
+
+      <HashRouter>
+        <Routes>
+          <Route element={<BodyContainer/>}>
+            <Route path='strona1' index element={<h1>1</h1>}/>
+            <Route path='strona2' index element={<h2>2</h2>}/>
+            <Route path='strona3' index element={<h3>3</h3>}/>
+            <Route path='*' index element={<Navigate to="strona1"></Navigate>}/>
+              
+          </Route>
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
